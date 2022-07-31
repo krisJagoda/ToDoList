3499d3fa501a46616e981be1395633eddb560022
Krystian Jagoda <krystian.jagoda@outlook.com>
2022-08-01 00:02:34 +0200

    Add fontawesome to project and add createToolsArea function

--- app.js
+++ app.js
@@ -22,12 +22,32 @@ const addNewTask = () => {
     if (inputVal.trim().length !== 0) {
         error.textContent = 'New task added'
         const todo = document.createElement('li')
+        const div = createToolsArea()
         todo.textContent = input.value
         ulList.append(todo)
+        todo.append(div)
         input.value = ''
     } else {
         error.textContent = 'No tasks added: add a new task'
     }
+}
+
+const createToolsArea = () => {
+    const div = document.createElement('div')
+    const btn1 = document.createElement('button')
+    const btn2 = document.createElement('button')
+    const btn3 = document.createElement('button')
+
+    div.setAttribute('class', 'tools')
+    btn1.setAttribute('class', 'complete')
+    btn1.innerHTML = '<i class="fas fa-check"></i>'
+    btn2.setAttribute('class', 'edit')
+    btn2.textContent = 'EDIT'
+    btn3.setAttribute('class', 'delete')
+    btn3.innerHTML = '<i class="fas fa-times"></i>'
+
+    div.append(btn1, btn2, btn3)
+    return div
 }
 
 document.addEventListener('DOMContentLoaded', main)
